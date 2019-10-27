d4fcec7c7cb2fa6c99660fc35106a20efd540f29
Sydney Hunter <sydney.jane.hunter@gmail.com>
2019-10-27 21:34:44 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,4 +1,4 @@
-def addition(num1=5, num2=4)
+def addition(num1, num2)
   num1 + num2
 end
 
@@ -7,17 +7,17 @@ def subtraction(num1, num2)
 end
 
 def division(num1, num2)
-
+  num1 / num2
 end
 
 def multiplication(num1, num2)
-
+  num1 * num2
 end
 
 def modulo(num1, num2)
-
+  num2 / num1
 end
 
 def square_root(num)
-
+  Math.sqrt (num)
 end
